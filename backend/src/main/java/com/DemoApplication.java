642de1203a52105bb101c62f870febedaeb94b81
umediTech <92442880+umediTech@users.noreply.github.com>
2022-02-26 13:40:51 -0500

    (initial) Setup Directory

--- backend/src/main/java/com/DemoApplication.java
+++ backend/src/main/java/com/DemoApplication.java
@@ -2,10 +2,11 @@ package com;
 
 import java.util.List;
 
+import com.BrushCircle.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.example.demo.demo.model.User;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -16,6 +17,6 @@ public class DemoApplication {
 
 	@GetMapping
 	public List<User> hello(){
-		return List.of(new User(1L, "Mariam", "Creator", "mariam.jones@gmail.com"));
+		return List.of(new User(1L, "mari1", "p@22w0rd", "Mariam", "Jones", "Creator", "mariam.jones@gmail.com"));
 	}
 }
